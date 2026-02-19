b75c9a48f2fe76ef9705ce07240dcfae813b5ced
Nithish HN <hnnithish@gmail.com>
2026-02-19 15:43:52 +0530

    Add files via upload

---
+++ Weather Application/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request
+import requests
+import os
+from dotenv import load_dotenv
+load_dotenv()
+app = Flask(__name__)
+
+API_KEY = os.getenv("API_KEY")
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    weather_data = None
+
+    if request.method == "POST":
+        city = request.form["city"]
+
+        url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+        response = requests.get(url)
+        data = response.json()
+
+        if response.status_code == 200:
+            weather_data = {
+                "city": city,
+                "temperature": data["main"]["temp"],
+                "description": data["weather"][0]["description"],
+                "humidity": data["main"]["humidity"],
+                "wind": data["wind"]["speed"]
+            }
+
+    return render_template("index.html", weather=weather_data)
+
+if __name__ == "__main__":
+    app.run(debug=True)
